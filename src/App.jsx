ac74dbf0dd7d67df9aeba29883421555885c198a
Nikhil Kumar <kumarnikhil302021@gmail.com>
2026-06-16 22:52:11 +0530

    Fixed App.jsx routing and added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/profile/:username" element={<Profile />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
